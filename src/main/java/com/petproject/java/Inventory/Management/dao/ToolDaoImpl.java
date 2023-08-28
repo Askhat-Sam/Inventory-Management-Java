70c486d31245078549d488a8116de22f3f1b3b1f
askhat.sam <askhat.sam@gmail.com>
2023-08-28 19:37:25 +0600

    feat: add entityManager

--- src/main/java/com/petproject/java/Inventory/Management/dao/ToolDaoImpl.java
+++ src/main/java/com/petproject/java/Inventory/Management/dao/ToolDaoImpl.java
@@ -1,11 +1,19 @@
 package com.petproject.java.Inventory.Management.dao;
 
 import com.petproject.java.Inventory.Management.enntity.Tool;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 @Repository
 public class ToolDaoImpl implements ToolDao{
+    private EntityManager entityManager;
+    @Autowired
+    public ToolDaoImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
     @Override
     public List<Tool> findAll() {
         return null;
