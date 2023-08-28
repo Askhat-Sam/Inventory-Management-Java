583c5c561eff315ea519aa24dd4c3df2f5fc9c98
askhat.sam <askhat.sam@gmail.com>
2023-08-28 21:58:48 +0600

    feat: add getmapping for the /list

--- src/main/java/com/petproject/java/Inventory/Management/service/ToolServiceImpl.java
+++ src/main/java/com/petproject/java/Inventory/Management/service/ToolServiceImpl.java
@@ -1,4 +1,46 @@
 package com.petproject.java.Inventory.Management.service;
 
+import com.petproject.java.Inventory.Management.dao.ToolRepository;
+import com.petproject.java.Inventory.Management.enntity.Tool;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+@Service
 public class ToolServiceImpl implements ToolService{
+
+    private ToolRepository toolRepository;
+
+    public ToolServiceImpl(ToolRepository toolRepository) {
+        this.toolRepository = toolRepository;
+    }
+
+    @Override
+    public List<Tool> findAll() {
+        return toolRepository.findAll();
+    }
+
+    @Override
+    public Tool findById(int theId) {
+        Optional<Tool> result = toolRepository.findById(theId);
+
+        Tool theTool = null;
+
+        if (result.isPresent()){
+            theTool = result.get();
+        } else {
+            throw new RuntimeException("Did not find tool id "+ theId);
+        }
+        return theTool;
+    }
+
+    @Override
+    public void save(Tool theTool) {
+        toolRepository.save(theTool);
+    }
+
+    @Override
+    public void deleteById(int theId) {
+        toolRepository.deleteById(theId);
+    }
 }
