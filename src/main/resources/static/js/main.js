9255276f0cbc61865cf6bc274609a012a8d90c1e
Askhat Sam <Askhat.Sam@gmail.com>
2023-10-25 07:53:46 +0600

    feat: add nav bar to admin-user.html

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,23 +1,23 @@
-//$('document').ready(function(){
-//    $('.table .btn-edit').on('click', function(e){
-//            e.preventDefault();
-//
-//            var href=$(this).attr('href')
-//
-//            console.log(href)
-//
-//            $.get(href, function(tool, status){
-//                 $('#idEdit').val(tool.id);
-//                $('#partNumberEdit').val(tool.partNumber);
-//                $('#serialNumberEdit').val(tool.serialNumber);
-//                $('#descriptionEdit').val(tool.description);
-//                $('#locationEdit').val(tool.location);
-//                console.log($('#locationEdit').val(tool.location));
-//            });
-//
-//            $('#editModal').modal('show');
-//     });
-//});
+$('document').ready(function(){
+    $('.table .btn-edit').on('click', function(e){
+            e.preventDefault();
+
+            var href=$(this).attr('href')
+
+            console.log(href)
+
+            $.get(href, function(tool, status){
+                 $('#idEdit').val(tool.id);
+                $('#partNumberEdit').val(tool.partNumber);
+                $('#serialNumberEdit').val(tool.serialNumber);
+                $('#descriptionEdit').val(tool.description);
+                $('#locationEdit').val(tool.location);
+                console.log($('#locationEdit').val(tool.location));
+            });
+
+            $('#editModal').modal('show');
+     });
+});
 
 $('document').ready(function(){
     $('.btn-edit-user').on('click', function(e){
