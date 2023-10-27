69f2371922cda81793b6f40c025853d25adaf395
Askhat Sam <Askhat.Sam@gmail.com>
2023-10-27 12:28:15 +0600

    feat: remove id field from the modal window for editing tool

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -7,7 +7,6 @@ $('document').ready(function(){
             console.log(href)
 
             $.get(href, function(tool, status){
-                 $('#idEdit').val(tool.id);
                 $('#partNumberEdit').val(tool.partNumber);
                 $('#serialNumberEdit').val(tool.serialNumber);
                 $('#descriptionEdit').val(tool.description);
